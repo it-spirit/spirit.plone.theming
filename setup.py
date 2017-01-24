da6d13a38a9f9d59fbca609c158a2c111b7d1d02
Thomas Massmann <thomas.massmann@it-spir.it>
2017-01-24 13:04:17 +0100

    Back to development: 0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
     setup,
 )
 
-version = '0.3'
+version = '0.4.dev0'
 description = "Theming extensions for Plone Websites."
 long_description = ('\n'.join([
     open('README.rst').read(),
